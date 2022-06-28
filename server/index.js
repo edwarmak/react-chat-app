8a6d6c48332bc2aca2294bb063f1d27e000e5ba8
Makayla Edwards <mackedwards@Makaylas-MacBook-Air.local>
2022-06-28 14:06:07 -0600

    basic join layout complete

--- server/index.js
+++ server/index.js
@@ -5,8 +5,21 @@ const cors = require('cors')
 // which port we are running on
 const PORT = process.env.PORT || 5001
 
+const router = require('./router')
+
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+
+// listen for incoming sockets and log to the console
+io.on('connection', (socket) => {
+    console.log('We have a new connection!')
+// listen for users disconnecting and log to the console
+    socket.on('disconnect', () => {
+        console.log('User has disconnected.')
+    })
+})
+
+app.use(router)
 
 server.listen(PORT, () => console.log(`server has started on port ${PORT}`))
