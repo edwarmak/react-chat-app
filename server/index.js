a31c73d6f4bccb883b768393c8d59a9594434ad4
Makayla Edwards <mackedwards@Makaylas-MacBook-Air.local>
2022-06-28 13:08:17 -0600

    backend running

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
+const cors = require('cors')
+// which port we are running on
+const PORT = process.env.PORT || 5001
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+
+server.listen(PORT, () => console.log(`server has started on port ${PORT}`))
